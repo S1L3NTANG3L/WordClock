85e1c4819642c11e9b2a2dd1fe615083bc1fecf9
Rechard Preston <rechard.preston@gmail.com>
2024-12-19 20:03:59 +0200

    Initial Commit

---
+++ Word Clock/Clock.py
@@ -0,0 +1,10 @@
+from machine import Pin
+from time import sleep
+
+gpio1 = Pin(0,Pin.OUT)
+
+while True:
+    gpio1.value(1)  # Turn on the pin
+    print(f"GPIO {pin} is now ON")  # Optional: print the status
+    sleep(1)  # Wait for 1 second
+    gpio1.value(0)  # Turn off the pin
